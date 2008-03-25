c0514902e9c68e43590e0de3055a3ed418182b12
Mike Perham <mperham@gmail.com>
2008-03-24 20:41:45 -0500

    Remove merb/sequel version

--- app/models/track.rb
+++
@@ -1,2 +0,0 @@
-class Track < Sequel::Model
-end
--- spec/controllers/tracks_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), "..", 'spec_helper.rb')
-
-describe Tracks, "index action" do
-  before(:each) do
-    dispatch_to(Tracks, :index)
-  end
-end
