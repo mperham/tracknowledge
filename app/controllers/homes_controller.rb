50a9259df68dc6987e14e0d4be7c72c2f66338dd
Mike Perham <mperham@gmail.com>
2008-04-07 22:31:04 -0500

    Handle null geocode result Add geocode.us as a backup provider

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,6 @@ class HomesController < ApplicationController
   geocode_ip_address
   
   def index
-    @closest = Track.find_within(500, :conditions => 'tracks.status = 1', :origin => current_location)
+    @closest = Track.find_within(500, :conditions => 'tracks.status = 1', :origin => current_location) if current_location
   end
 end
