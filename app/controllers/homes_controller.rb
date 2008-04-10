8e52a998d40989f5be04d2f83454e99809873830
Mike Perham <mperham@gmail.com>
2008-04-10 09:36:37 -0500

    Update query

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,6 +4,6 @@ class HomesController < ApplicationController
 
   def index
     @closest = Track.find_within(500, :conditions => 'tracks.status = 1', :origin => current_location) if current_location
-    @submitters = Track.connection.select_rows('select added_by, count(*) from tracks group by added_by limit 5')
+    @submitters = Track.connection.select_rows('select added_by, count(*) from tracks where status = 1 group by added_by order by count(*) DESC limit 5')
   end
 end
