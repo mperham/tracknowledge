60013b209e58acdebff72d5df6ecbcf253729737
Mike Perham <mperham@gmail.com>
2008-04-13 15:30:04 -0500

    Small style tweak

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,6 +6,7 @@ class HomesController < ApplicationController
     Track.cache do
       @closest = Track.find_within(200, :conditions => 'tracks.status = 1', :origin => current_location, :order => 'distance') if current_location
       @submitters = Track.connection.select_rows('select added_by, count(*) from tracks where status = 1 and added_by <> \'\' group by added_by order by count(*) DESC limit 5')
+#      @track_count = Track.count(:conditions => 'tracks.status = 1')
     end
   end
 end
