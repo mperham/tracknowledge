0a2bf3008d5e38f6aa4ac692cbd5f7e5baa11ae2
Mike Perham <mperham@gmail.com>
2008-04-18 21:38:35 -0500

    Add Flickr photo support. Integrates flickr-fu 0.1.4 with some bug fixes.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,9 @@
 class TracksController < ApplicationController
   caches_page :show
   caches_page :new
+  
+  FLICKR = Flickr.new("#{ENV['HOME']}/.flickr.yml")
+  
 
   def new
     @track = Track.new
@@ -40,9 +43,26 @@ class TracksController < ApplicationController
   def show
     @track = Track.find(params[:id], :include => [:track_blob, :categories])
     @map = map_for @track
+    @videos = find_video_for @track
+    @pictures = find_photos_for @track
   end
   
   private
+  
+  def find_video_for(track)
+    search = Youtube::Video.find(:first, :params => {:vq => @track.name, :"max-results" => '5'})
+    (search ? search.entry : [])
+  end
+  
+  def find_photos_for(track)
+#    lat, lng = track.lat, track.lng
+    pics = FLICKR.photos.search(:text => CGI::escape(track.name), :per_page => 10, :min_upload_date => 1.year.ago.to_i)
+    # Flickr's bounding box does not seem to work
+#                                :bbox => "#{lng-0.1},#{lat+0.1},#{lng+0.1},#{lat+0.1}")
+
+    # Flickr's results can return pics without thumbnails or original URLs.  Need to prune these.
+    pics.delete_if { |pic| !pic.url(:thumbnail) || !pic.url(:photopage) }[0..4]
+  end
   
   def notify_admin(track)
     begin
--- config/initializers/service_mocks.rb
+++ config/initializers/service_mocks.rb
@@ -1,4 +1,14 @@
-if RAILS_ENV == 'development'
+module Flickr
+  class Base
+    def sign_request(foo)
+      # override this as it doesn't seem to work in flickr-fu 0.1.4.
+      # The only request we make is flickr.photos.search which does not require authentication so it's not important
+    end
+  end
+end
+
+
+if RAILS_ENV != 'production'
   require 'ostruct'
   module Youtube
     class Video
@@ -13,6 +23,27 @@ if RAILS_ENV == 'development'
             :title => "Video #{idx}")
         end
         search
+      end
+    end
+  end
+
+  class Photo
+    def url(something)
+      "http://perham.net/mike/flickr.pic"
+    end
+    def title
+      "Picture"
+    end
+  end
+
+  module Flickr
+    class Photos
+      def search(*args)
+        photos = []
+        5.times do |idx|
+          photos << ::Photo.new
+        end
+        photos
       end
     end
   end
