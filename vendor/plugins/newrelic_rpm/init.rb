25781ef64c8fc866d6307e4baa6ee4a5d82afb47
Mike Perham <mperham@gmail.com>
2008-09-03 20:06:50 +0200

    Fix warnings

--- vendor/plugins/newrelic_rpm/init.rb
+++ vendor/plugins/newrelic_rpm/init.rb
@@ -61,8 +61,8 @@ begin catch (:disabled) do
     helper_path = File.join(File.dirname(__FILE__), 'ui', 'helpers')
     $LOAD_PATH << controller_path
     $LOAD_PATH << helper_path
-    Dependencies.load_paths << controller_path
-    Dependencies.load_paths << helper_path
+    ActiveSupport::Dependencies.load_paths << controller_path
+    ActiveSupport::Dependencies.load_paths << helper_path
     config.controller_paths << controller_path
     require 'newrelic_routing'
     
