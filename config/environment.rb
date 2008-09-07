bc738ec8b0294d8ed5c5dcccedf327e23eac9d27
Mike Perham <mperham@gmail.com>
2008-09-07 15:24:47 -0500

    Remove active-youtube

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,6 @@ RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require 'active_youtube'
 require 'flickr_fu'
 
 Rails::Initializer.run do |config|
