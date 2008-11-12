8809a6e17c85938a121aa3e37c34dfbf09f06e70
Mike Perham <mperham@gmail.com>
2008-11-12 15:13:19 -0600

    More threadsafe changes

--- config/environment.rb
+++ config/environment.rb
@@ -61,6 +61,7 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   config.active_record.default_timezone = :utc
+  config.threadsafe!
 end
 
 ExceptionNotifier.exception_recipients = %w(mike@tracknowledge.org)
