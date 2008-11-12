c03d7767266eec56b73f86a0b7fef832f8880883
Mike Perham <mperham@gmail.com>
2008-11-12 14:13:51 -0600

    Fix GeoKit issue under Jruby

--- vendor/plugins/geokit/lib/geo_kit/acts_as_mappable.rb
+++ vendor/plugins/geokit/lib/geo_kit/acts_as_mappable.rb
@@ -377,20 +377,22 @@ module GeoKit
           lat = deg2rad(origin.lat)
           lng = deg2rad(origin.lng)
           multiplier = units_sphere_multiplier(units)
-          case connection.adapter_name.downcase
-          when "sqlite"
+          adapter = connection.adapter_name.downcase
+          adapter = connection.config[:adapter].downcase if adapter == 'jdbc' # get at the underlying driver if this is JDBC/jruby
+          case adapter
+          when /sqlite/
             sql=<<-SQL_END 
                   (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
                   COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
                   SIN(#{lat})*SIN(RADIANS(#{qualified_lat_column_name}))))*#{multiplier})
                   SQL_END
-          when "mysql"
+          when /mysql/
             sql=<<-SQL_END 
                   (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
                   COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
                   SIN(#{lat})*SIN(RADIANS(#{qualified_lat_column_name}))))*#{multiplier})
                   SQL_END
-          when "postgresql"
+          when /postgresql/
             sql=<<-SQL_END 
                   (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
                   COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
@@ -406,18 +408,20 @@ module GeoKit
         def flat_distance_sql(origin, units)
           lat_degree_units = units_per_latitude_degree(units)
           lng_degree_units = units_per_longitude_degree(origin.lat, units)
-          case connection.adapter_name.downcase
-          when "sqlite"
+          adapter = connection.adapter_name.downcase
+          adapter = connection.config[:adapter].downcase if adapter == 'jdbc' # get at the underlying driver if this is JDBC/jruby
+          case adapter
+          when /sqlite/
             sql=<<-SQL_END
                   SQRT(POW(#{lat_degree_units}*(#{origin.lat}-#{qualified_lat_column_name}),2)+
                   POW(#{lng_degree_units}*(#{origin.lng}-#{qualified_lng_column_name}),2))
                   SQL_END
-          when "mysql"
+          when /mysql/
             sql=<<-SQL_END
                   SQRT(POW(#{lat_degree_units}*(#{origin.lat}-#{qualified_lat_column_name}),2)+
                   POW(#{lng_degree_units}*(#{origin.lng}-#{qualified_lng_column_name}),2))
                   SQL_END
-          when "postgresql"
+          when /postgresql/
             sql=<<-SQL_END
                   SQRT(POW(#{lat_degree_units}*(#{origin.lat}-#{qualified_lat_column_name}),2)+
                   POW(#{lng_degree_units}*(#{origin.lng}-#{qualified_lng_column_name}),2))
